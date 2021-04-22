e9223ba28cec15d6d1cd70525e6180be91cc9775
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-22 08:33:49 +0000

    Nav bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="w-full bg-gray-700 absolute top-0 left-0 min-h-full">
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+
+      <div @click='handleToggle' class='w-16 h-12 border-2 border-blue-500'>
+          Toggle close
+        </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    methods:{
+        handleToggle(){
+            this.$emit('closeNav')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
